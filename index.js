ca0c89019cf6f035905461d05eb9f1b97801d788
jzolo22 <jzolotarev@gmail.com>
2020-12-01 11:35:09 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// ---------------------------Page Scroll--------------------------------- //
+
+const whiteArrow = document.querySelector(".arrow.down")
+const blackArrow = document.querySelector(".arrow.down.black")
+
+
+const scroll = () => {
+    const pageTwo = document.querySelector(".bg2");
+    pageTwo.scrollIntoView({behavior: "smooth"});
+}
+
+const scrollTwo = () => {
+    const pageThree = document.querySelector(".bg2.page-three");
+    pageThree.scrollIntoView({behavior: "smooth"});
+}
+
+whiteArrow.addEventListener("click", scroll)
+blackArrow.addEventListener("click", scrollTwo)
+
+// ------------------------------------------------------------------------- //
