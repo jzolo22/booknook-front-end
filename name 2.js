d5489cd61faa4f6bb23173b1faa4aa5eaea4b306
jzolo22 <jzolotarev@gmail.com>
2020-12-04 14:27:45 -0500

    refactored

---
+++ name 2.js
@@ -0,0 +1,38 @@
+const nameForm = document.querySelector("#username-form")
+const h2 = document.querySelector("h2")
+
+let name
+let userId
+
+
+nameForm.addEventListener("submit", submitName)
+
+function createUser(name) {
+
+    fetch("http://localhost:3000/users", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        }, 
+        body: JSON.stringify(newUser)
+    })
+        .then(r => r.json())
+        .then(newUserObj => {
+            userId = newUserObj.id
+        })
+}
+
+
+function submitName(event) {
+    event.preventDefault()
+    name = nameForm.name.value
+    h2.textContent = `come on in, ${name}`
+
+    newUser = {
+        username: name
+    }
+
+    createUser(name)
+
+    nameForm.reset()
+}
